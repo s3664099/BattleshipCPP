a09e92fc09abdbd9e5d12de9e76bbf68050500c0
Dave Sarkies <s3664099@student.rmit.edu.au>
2024-01-18 20:54:05 +1100

    Completed fire function and mostly compiled

--- action.cpp
+++ action.cpp
@@ -1,6 +1,7 @@
 int fire(Board* defender, Board* attacker) {
 
 	int result = 0;
+	int selectShot;
 
 	//Sets the variables used in the algorithm including information from the previous turn
 	Coordinate originalHit(0,0);
@@ -44,8 +45,8 @@ int fire(Board* defender, Board* attacker) {
 	} else if (hitShip ==2) {
 
 		//Retrieves the next shot
-		Set<int> shipShots = defender->getShipShots();
-		Coordinate hit = defender.getHit();
+		set<int> shipShots = defender->getShipShots();
+		Coordinate hit = defender->getHit();
 		selectShot = defender->getMovement();
 		shot = getNextShot(selectShot,hit);
 
@@ -58,7 +59,7 @@ int fire(Board* defender, Board* attacker) {
 		//Otherwise selects a random shot from the list of potential shots
 		int shotSelected = rand()%potentialShots.size()-1;
 
-		vector<Coordinate> ptshList(potentialShots);
+		vector<Coordinate> ptshList(potentialShots.begin(),potentialShots.end());
 
 		if (potentialShots.size()>0) {
 			potentialShots = removeShot(potentialShots,shot);
@@ -67,29 +68,29 @@ int fire(Board* defender, Board* attacker) {
 
 	attacker->setPotentialShots(potentialShots);
 
-	char hitPosition = '0';
+	string hitPosition = "0";
 
 	std::cout<<"Shot fired at: "<<shot.getX()<<","<<shot.getY()<<std::endl;
 
 	//Has this empty spot already been hit
-	if ((grid*)[shot.getX()][shot.getY()] == "0") {
-		(grid*)[shot.getX()][shot.getY()] == ".";
+	if ((*grid)[shot.getX()][shot.getY()] == "0") {
+		(*grid)[shot.getX()][shot.getY()] == ".";
 	}
 
-	if ((grid*)[shot.getX()][shot.getY()] != ".") {
+	if ((*grid)[shot.getX()][shot.getY()] != ".") {
 		
 		//Marks it as a ship
 		hitPosition = "X";
 
 		//Sets the flag that a ship has been hit and checks if it has been sunk
-		defender->hitShip(shot);
+		defender->setHit(shot);
 		hitResult = defender->checkWhichShip(shot,attacker->getName());
 
 		std::cout<<"Hit result "<<hitResult<<std::endl;
 
 		if (hitShip == 0) {
 			defender->setHitShip(1);
-			Set<int> shipShots = checkShitShots(potentialShots,shot,defender);
+			set<int> shipShots = checkShitShots(potentialShots,shot,defender);
 			defender->setShipShots(shipShots);
 		}
 
@@ -98,69 +99,54 @@ int fire(Board* defender, Board* attacker) {
 			defender->setHitShip(2);
 			defender->setOriginalHit(originalHit);
 
-			set<int> shipShots = new set<int>();
+			set<int> shipShots;
 
 			//Sets the next potential shot in case the shot misses
 			if(selectShot == 0) {
-				shipShots.add(1);
+				shipShots.insert(1);
 			} else if (selectShot == 1) {
-				shipShots.add(0);
+				shipShots.insert(0);
 			} else if (selectShot == 2) {
-				shipShots.add(3);
+				shipShots.insert(3);
 			} else {
-				shipShots.add(2);
+				shipShots.insert(2);
 			}
 
 			//Checks if the next shot will be off the board
-
-		}
-	}	
-
-
-	/*
-	public int fire(Board defender, Board attacker) {
-		
-		//The spot isn't empty
-		if (grid[shot.getX()][shot.getY()] != ".") {
-									
-			//If it has, sets it that it has been hit more than once
-			if (hitShip == 1) {
-				
-				//Checks if the next shot will be off the board
-				if ((selectShot == 0) && (shot.getX()+1>defender.getSize()-1)) {
-					defender.setMovement(1);
-				} else if ((selectShot == 1) && (shot.getX()+1<0)) {
-					defender.setMovement(0);
-				} else if ((selectShot == 2) && (shot.getY()+1>defender.getSize()-1)) {
-					defender.setMovement(3);
-				} else if ((selectShot == 3) && (shot.getY()+1<0)) {
-					defender.setMovement(2);
-				}
-				
-				defender.setShipShots(shipShots);
+			if ((selectShot == 0) && (shot.getX()+1>defender->getSize()-1)) {
+				defender->setMovement(1);
+			} else if ((selectShot == 1) && (shot.getX()+1<0)) {
+				defender->setMovement(0);
+			} else if ((selectShot == 2) && (shot.getY()+1>defender->getSize()-1)) {
+				defender->setMovement(3);
+			} else if ((selectShot == 4) && (shot.getY()+1<0)) {
+				defender->setMovement(2);
 			}
-		
-		//If it was a miss
-		} else {
-			
-			//If the previous shots hit the ship, then goes back to the original spot
-			//and sets it in the opposite direction
-			if (hitShip == 2) {
-				defender.setHitShip(1);
-				defender.setHit(defender.getOriginalHit());
-			} 
+
+			defender->setShipShots(shipShots);
 		}
-		
-		grid[shot.getX()][shot.getY()] = hitPosition;
-		spotsHit[shot.getX()][shot.getY()] = hitPosition;
-		
-		if (hitResult == 2) {
-			defender.setHitShip(0);
+
+	//If it was a miss
+	} else {
+
+		//If the previous shots hit the ship, then goes back to the original spot
+		//and sets it in the opposite direction
+		if (hitShip == 2) {
+			defender->setHitShip(1);
+			defender->setHit(defender->getOriginalHit());
 		}
-		
-		return hitResult;
 	}
-		
+
+	(*grid)[shot.getX()][shot.getY()] == hitPosition;
+	(*spotsHit)[shot.getX()][shot.getY()] == hitPosition;
+
+	if (hitResult == 2) {
+		defender->setHitShip(0);
+	}
+
+	return hitResult;
+}
+		/*
 	//Method that takes a set and a coordinate and removes it from the set
 	private Set<Coordinate> removeShot(Set<Coordinate> potentialShots,Coordinate shot) {
 		
